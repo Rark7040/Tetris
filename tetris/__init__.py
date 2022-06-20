54cbc52075ff9d1c2a009703f21a93c97782cff1
Rark7040 <crimera.hello@gmail.com>
2022-06-20 16:04:43 +0900

    init

---
+++ tetris/__init__.py
@@ -0,0 +1,4 @@
+__title__ = "Tetris"
+__author__ = "RarkHopper"
+__license__ = "MIT"
+__version__ = "1.0.0"
---
+++ tetris/__main__.py
@@ -0,0 +1,6 @@
+def main():
+    print('noop')
+
+
+if __name__ == '__main__':
+    main()
