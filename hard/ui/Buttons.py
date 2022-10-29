c885ec81b4d4e7170bac8272c4fb58236dd339b0
Rark7040 <crimera.hello@gmail.com>
2022-10-29 18:39:49 +0900

    bug fix...

--- hard/ui/Buttons.py
+++ hard/ui/Buttons.py
@@ -5,8 +5,8 @@ class Buttons(Enum):
 
     # 1 is pressed, 0 is released
     RIGHT_ROTATE: int = 0b1
-    LEFT_ROTATE: int = 0b01
-    RIGHT_MOVE: int = 0b001
-    LEFT_MOVE: int = 0b0001
-    DOWN_MOVE: int = 0b00001
-    RESET: int = 0b000001
+    LEFT_ROTATE: int = 0b10
+    RIGHT_MOVE: int = 0b100
+    LEFT_MOVE: int = 0b1000
+    DOWN_MOVE: int = 0b10000
+    RESET: int = 0b100000
