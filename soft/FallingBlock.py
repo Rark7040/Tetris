b149b65b9409c0bd6d56c16e011563ba24c33dc2
Rark7040 <crimera.hello@gmail.com>
2022-11-17 18:17:39 +0900

    v2d

--- soft/FallingBlock.py
+++ soft/FallingBlock.py
@@ -1,10 +1,33 @@
+import copy
+
+from soft import Vector2d
 from soft.BitBlock import BitBlock
 
 
 class FallingBlock(BitBlock):
+    MAX_LIFE_TIME = 20
 
-    def __init__(self):
+    def __init__(self, v2d: Vector2d):
         super().__init__()
-        self.__x: int = 0
-        self.__y: int = 0
+        self.lifetime = self.MAX_LIFE_TIME
+        self.v2d: Vector2d = v2d
 
+    def reset_life(self):
+        self.lifetime: int = self.MAX_LIFE_TIME
+
+    def falling(self):
+        self.v2d.y -= 1
+        self.on_move()
+
+    def on_colliding(self):
+        self.lifetime -= 1
+
+    def on_move(self):
+        self.reset_life()
+        # and more ...?
+
+    def is_controllable(self) -> bool:
+        return 0 < self.lifetime
+
+    def get_cloning_vector(self) -> Vector2d:
+        return copy.deepcopy(self.v2d)
